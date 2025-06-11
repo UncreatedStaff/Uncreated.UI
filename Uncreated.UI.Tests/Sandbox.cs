f0f5fac94d478d0a78b5d83c40767529442ad44a
Daniel Willett <dannywill8@gmail.com>
2025-06-11 17:02:32 -0400

    Added sandbox test

---
+++ Uncreated.UI.Tests/Sandbox.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks;
+using Uncreated.Framework.UI;
+using Uncreated.Framework.UI.Data;
+using Uncreated.Framework.UI.Patterns;
+using Uncreated.Framework.UI.Reflection;
+
+namespace Uncreated.UI.Tests;
+
+public class Sandbox
+{
+    [Test]
+    public void SandboxTest()
+    {
+        Console.WriteLine("hello");
+    }
+    // put your UI here
+}
